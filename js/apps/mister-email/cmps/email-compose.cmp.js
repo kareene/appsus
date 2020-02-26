498de917244db0841d369e09f3ca2519cb73a17f
Kareene Komet <kareene@kareene.com>
2020-02-26 19:56:26 +0200

    need to move

--- js/apps/mister-email/cmps/email-compose.cmp.js
+++ js/apps/mister-email/cmps/email-compose.cmp.js
@@ -1,11 +1,22 @@
 export default {
     template: `
         <section class="email-compose">
-            <h3>compose</h3>
-            <form>
-                <input />
+            <header>New Message</header>
+            <section class="input-container">
+                <input placeholder="To:" />
+                <input placeholder="Subject" />
                 <textarea></textarea>
-            </form>
+            </section>
+            <section class="button-container flex space-between">
+                <button @click="sendEmail" class="send-btn" title="Send email">Send</button>
+                <button @click="deleteDraft" class="delete-btn" title="Discard draft">
+                    <i class="fas fa-trash-alt"></i>
+                </button>
+            </section>
         </section>
-    `
+    `,
+    methods: {
+        sendEmail() {},
+        deleteDraft() {}
+    }
 };
