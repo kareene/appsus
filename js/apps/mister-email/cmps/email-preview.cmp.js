dbb22ac1629c976ef3c58cab76fe10e198821b7f
limor ala <limorelv@gmail.com>
2020-02-26 17:18:45 +0200

    expend mail preview

--- js/apps/mister-email/cmps/email-preview.cmp.js
+++ js/apps/mister-email/cmps/email-preview.cmp.js
@@ -1,8 +1,37 @@
 export default {
     template: `
-        <h3>{{email.subject}}</h3>`,
-   
+    <li class = "email-preview flex space-between" :class="previewSize"  @click= "toggleExpended">
+        <h3>{{email.subject}}</h3>
+        <p>{{formattedBody}}</p>
+        <p>{{formattedTime}}</p>
+    </li>`,
+    
     props: ['email'],
+
+    data(){
+        return {
+            isExpended : false
+        }
+    },
+
+    computed: {
+        formattedTime(){
+            var date = new Date(this.email.sentAt);
+            return date.toLocaleString();
+        },
+        formattedBody(){
+            return this.email.body.substring(0,99) + '...';
+        },
+        previewSize(){
+            return (this.isExpended) ? 'expended' : 'small';
+        }
+    },
+    methods: {
+        toggleExpended(){
+            console.log("toggle expended")
+            this.isExpended = !this.isExpended;
+        }
+    }
     
     
 };
