f4026691143ecf7e73dbcf66be64eb2468f9cd84
Kareene Komet <kareene@kareene.com>
2020-02-28 11:32:00 +0200

    video

--- js/apps/miss-keep/cmps/note-video.cmp.js
+++ js/apps/miss-keep/cmps/note-video.cmp.js
@@ -1,24 +1,30 @@
 export default {
     template: `
         <article class="note-video">
-            <h1>test</h1>
-            <div class="video-container">
-                <iframe
-                    :src="https://www.youtube.com/embed/youtubeVideoId"
-                    frameborder="0" allowfullscreen
-                ></iframe>
+            <h3>{{info.title}}</h3>
+            <div v-if="videoUrl" class="video-container">
+                <iframe :src="videoUrl" frameborder="0" allowfullscreen></iframe>
             </div>
+            <p v-else>Sorry, something went wrong while loding the video.</p>
         </article>
     `,
     props: ['info'],
-    computed: {
-        youtubeVideoId() {
-            const youtube_regex = /^.(youtu\.be\/|vi?\/|u\/\w\/|embed\/|\?vi?=|\&vi?=)([^#\&\?]).*/i;
-            videoId = this.info.url.match(youtube_regex)[2];
-            return (videoId && videoId.length === 11) ? videoId : '';
+    data() {
+        return {
+            videoUrl: ''
+        }
+    },
+    created() {
+        const videoId = this.getYoutubeVideoId(this.info.url);
+        this.videoUrl = (videoId) ? `https://www.youtube.com/embed/${videoId}` : '';
+    },
+    methods: {
+        getYoutubeVideoId(url) {
+            console.log(url)
+            // RegEx by Stephan Schmitz <eyecatchup@gmail.com>
+            // https://stackoverflow.com/a/10315969/624466
+            const youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+            return url.match(youtubeRegex)[1];
         }
     }
 }
-
-// aspect ratio:  width * (9/16, 56.25%)
-// allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
