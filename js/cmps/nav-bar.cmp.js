b5b695199b06c49a6ae6ecd5188867f49689325a
limor ala <limorelv@gmail.com>
2020-03-03 07:59:26 +0200

    removed miss books

--- js/cmps/nav-bar.cmp.js
+++ js/cmps/nav-bar.cmp.js
@@ -3,13 +3,13 @@ export default {
         <nav class="navbar flex" @click = "closeScreen">
             <router-link to="/" exact> Home </router-link>
             
-            <router-link to="/book" > Miss Books </router-link>
+           
             
             <router-link to="/email" > Mister Email </router-link>
             
             <router-link to="/keep" > Miss Keep </router-link>
             
-            <router-link to="/about"> About </router-link>
+            
         </nav>
     `,
     // methods: {
