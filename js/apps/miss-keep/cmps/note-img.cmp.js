273932e3cec1aac6186d97270a184c24ce59edec
Kareene Komet <kareene@kareene.com>
2020-03-03 02:20:35 +0200

    note add css

--- js/apps/miss-keep/cmps/note-img.cmp.js
+++ js/apps/miss-keep/cmps/note-img.cmp.js
@@ -1,8 +1,10 @@
 export default {
     template: `
         <article class="note-img">
-            <p class="title">{{info.title}}</p>
-            <img :src="info.url" />
+            <p v-if="!info.title && !info.url" class="empty-note">Empty note</p>
+            <p v-if="info.title" class="title">{{info.title}}</p>
+            <img v-if="info.url" :src="info.url" />
+            <p v-else>Sorry, something went wrong while loding the image.</p>
         </article>
     `,
     props: ['info']
--- js/apps/miss-keep/cmps/note-todos.cmp.js
+++ js/apps/miss-keep/cmps/note-todos.cmp.js
@@ -1,6 +1,7 @@
 export default {
     template: `
         <article class="note-todos">
+            <p v-if="!info.title && !info.todos" class="empty-note">Empty note</p>
             <p v-if="info.title" class="title">{{info.title}}</p>
             <ul v-if="info.todos" class="clean-list">
                 <li v-for="todo in info.todos">
